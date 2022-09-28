b71facb9cf6c3e4f2cb2cfbdaa1381e3502ffded
ekrem <ekremufacik07@gmail.com>
2022-09-28 23:26:43 +0300

    change

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let submit = document.querySelector(".btn");
-let input = document.querySelector("#input");
-let message = document.querySelector(".message");
+const submit = document.querySelector(".btn");
+const input = document.querySelector("#input");
+const message = document.querySelector(".message");
+const countries = document.querySelector(".countries");
 let countryList = [];
 
 submit.addEventListener("click", () => {
@@ -13,16 +14,17 @@ submit.addEventListener("click", () => {
       .then((res) => {
         if (!res.ok) {
           throw new Error("Something went wrong !");
-        } else if (countryList.includes(input.value)) {
-          console.log(input.value);
+        } else if (countryList.includes(input.value.toLowerCase())) {
           throw new Error(
             `You already enter ${
-              input.value.charAt(0).toUpperCase() + input.value.slice(1)
+              input.value.charAt(0).toUpperCase() +
+              input.value.slice(1).toLowerCase()
             } !`
           );
         }
         return res.json();
       })
+
       .then((data) => updateDom(data))
       .catch((error) => {
         message.innerHTML = error;
@@ -32,20 +34,21 @@ submit.addEventListener("click", () => {
 });
 
 function updateDom(d) {
-  const countries = document.querySelector(".countries");
   const {
     main: { temp },
     weather,
   } = d;
   countries.innerHTML += `
   <div class="cards">
-    <h5>${input.value.charAt(0).toUpperCase() + input.value.slice(1)}</h5>
+    <h5>${
+      input.value.charAt(0).toUpperCase() + input.value.slice(1).toLowerCase()
+    }</h5>
     <p class="temp">${Math.round(temp)}<span>°C</span></p>
     <img src="./img/${weather[0].description}.png" width="80px" alt="">
     <p class="w-desc">${weather[0].description.toUpperCase()}</p>
   </div>
   `;
-  countryList.push(input.value);
+  countryList.push(input.value.toLowerCase());
   input.value = "";
   message.innerHTML = "";
 }
